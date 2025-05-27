35b8281beda5419862282f16edc69e5071032666
Respresso <respresso@debian.W>
2025-05-27 19:00:09 +0300

    EventDetails

---
+++ src/pages/EventDetails.jsx
@@ -0,0 +1,11 @@
+// src/pages/EventDetails.jsx
+function EventDetails() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-bold mb-2">Детали мероприятия</h2>
+      <p>Здесь будет подробная информация о выбранном мероприятии.</p>
+    </div>
+  );
+}
+
+export default EventDetails;
