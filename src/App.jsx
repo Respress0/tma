81b0b63e7f7d21129b7f2ad65eb2b950cc9f4dab
Respresso <respresso@debian.W>
2025-06-02 17:39:14 +0300

    feat: добавлен базовый дашборд с профилем Telegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-// App.jsx
-import React from "react";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1 bg-gray-100 min-h-screen">
-        <Header />
-        <Dashboard />
-      </main>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Dashboard />
     </div>
   );
 }
