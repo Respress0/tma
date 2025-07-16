8b754a0076bffd9ea2431e86567aa04e9a860249
Respresso <respresso@RESPRESSO.dl>
2025-07-16 20:18:20 +0300

    add tabbar

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -1,26 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const TabBar = ({ activeTab, setActiveTab }) => {
   return (
     <div className="tab-bar">
       <Link
         to="/events"
-        className={`tab-button ${activeTab === 'events' ? 'active' : ''}`}
-        onClick={() => setActiveTab('events')}
+        className={`tab-button ${activeTab === "events" ? "active" : ""}`}
+        onClick={() => setActiveTab("events")}
       >
         Мероприятия
       </Link>
       <Link
         to="/create"
-        className={`tab-button ${activeTab === 'create' ? 'active' : ''}`}
-        onClick={() => setActiveTab('create')}
+        className={`tab-button ${activeTab === "create" ? "active" : ""}`}
+        onClick={() => setActiveTab("create")}
       >
         Создать
       </Link>
       <Link
         to="/account"
-        className={`tab-button ${activeTab === 'account' ? 'active' : ''}`}
-        onClick={() => setActiveTab('account')}
+        className={`tab-button ${activeTab === "account" ? "active" : ""}`}
+        onClick={() => setActiveTab("account")}
       >
         Аккаунт
       </Link>
